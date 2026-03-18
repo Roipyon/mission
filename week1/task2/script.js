4696c0a56933c541e6f764bdbd42e2d121b1cceb
Roipyon <ouye1211@gmail.com>
2026-03-18 23:42:23 +0800

    finish function that recover the delected things

--- week1/task2/script.js
+++ week1/task2/script.js
@@ -14,11 +14,11 @@ document.addEventListener('DOMContentLoaded',()=>{
     {
         // 获取本地事件并对象化
         const contentObj = JSON.parse(localStorage.getItem(i));
-        if (!contentObj) return;
+        if (!contentObj) continue;
         const content = contentObj.title;
         const id = contentObj.id;
         // 判断待办事项
-        if (content.length === 0 || contentObj.status != 'do') return;
+        if (content.length === 0 || contentObj.status != 'do') continue;
         count = i+1;
         const newThing = document.createElement('div');
         thing.insertBefore(newThing, document.querySelector('#thing div:first-child'));
@@ -98,34 +98,75 @@ thing.addEventListener('change', e => {
     if (e.target.checked)
     {
         const next_p = e.target.nextElementSibling;
+        console.dir(next_p);
         next_p.style.color = "gray";
         next_p.style.textDecoration = "line-through solid";
         next_p.nextElementSibling.outerHTML = '';
         e.target.disabled = 'true';
-        localStorage.removeItem(id);
+        // localStorage.removeItem(id); 可选择直接删除事件
+        localStorage.setItem(id,JSON.stringify({
+            id: id,
+            title: next_p.innerText,
+            status: 'did'
+        }));
     }
 });
 
-binBtn.addEventListener('click',()=>{
-    bin.style.display = 'block';
-    thing.style.display = 'none';
-    for (let i=0;i<=10;i++)
+// 回收站
+binBtn.addEventListener('click',(e)=>{
+    console.log(e);
+    if (e.target.tagName === 'svg')
     {
-        // 获取本地事件并对象化
-        const contentObj = JSON.parse(localStorage.getItem(i));
-        if (!contentObj) return;
-        const content = contentObj.title;
-        const id = contentObj.id;
-        // 判断待办事项
-        if (content.length === 0 || contentObj.status != 'did') return;
-        const newThing = document.createElement('div');
-        thing.insertBefore(newThing, document.querySelector('#bin div:first-child'));
-        newThing.outerHTML = `
-            <div class="normal" data-id=${id}>
-                <input type="checkbox" class="select">
-                <p>${content}</p>
-                <p class="recover">恢复</p>
-            </div>
-            `;
+        e.target.parentNode.innerHTML = '<span style="cursor: pointer">确定</span>';
+        bin.style.display = 'block';
+        thing.style.display = 'none';
+        for (let i=0;i<=10;i++)
+        {
+            // 获取本地事件并对象化
+            const contentObj = JSON.parse(localStorage.getItem(i));
+            if (!contentObj) continue;
+            const content = contentObj.title;
+            const id = contentObj.id;
+            // 判断待办事项
+            if (content.length === 0 || contentObj.status != 'did') continue;
+            const newThing = document.createElement('div');
+            bin.insertBefore(newThing, document.querySelector('#bin div:first-child'));
+            newThing.outerHTML = `
+                <div class="normal" data-id=${id}>
+                    <input type="checkbox" checked disabled>
+                    <p style="color: gray;text-decoration: line-through solid">${content}</p>
+                    <p style="font-size: 13px;cursor: pointer">恢复</p>
+                </div>
+                `;
+        }
+    }
+    else if (e.target.tagName === 'SPAN')
+    {
+        e.target.parentNode.innerHTML = `
+        <svg class="icon" style="cursor: pointer;width: 2em;height: 2em;vertical-align: middle;fill: currentColor;overflow: hidden;" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="11862">
+                <path d="M842.666667 981.333333H181.333333a53.393333 53.393333 0 0 1-53.333333-53.333333V277.333333a21.333333 21.333333 0 0 1 42.666667 0v650.666667a10.666667 10.666667 0 0 0 10.666666 10.666667h661.333334a10.666667 10.666667 0 0 0 10.666666-10.666667V277.333333a21.333333 21.333333 0 0 1 42.666667 0v650.666667a53.393333 53.393333 0 0 1-53.333333 53.333333z m-181.333334-128a21.333333 21.333333 0 0 1-21.333333-21.333333V362.666667a21.333333 21.333333 0 0 1 42.666667 0v469.333333a21.333333 21.333333 0 0 1-21.333334 21.333333z m-298.666666 0a21.333333 21.333333 0 0 1-21.333334-21.333333V362.666667a21.333333 21.333333 0 0 1 42.666667 0v469.333333a21.333333 21.333333 0 0 1-21.333333 21.333333z m554.666666-682.666666H106.666667a21.333333 21.333333 0 0 1 0-42.666667h192v-32a53.393333 53.393333 0 0 1 53.333333-53.333333h320a53.393333 53.393333 0 0 1 53.333333 53.333333v32h192a21.333333 21.333333 0 0 1 0 42.666667z m-576-42.666667h341.333334v-32a10.666667 10.666667 0 0 0-10.666667-10.666667H352a10.666667 10.666667 0 0 0-10.666667 10.666667z" fill="#333333" p-id="11863">
+                </path>
+        </svg>`;
+        bin.style.display = 'none';
+        thing.style.display = 'flex';
+        bin.innerHTML = `
+            <div id="head1">
+            </div>`;
+        location.reload();
+    }
+});
+
+// 恢复事件
+bin.addEventListener('click',(e)=>{
+    if (e.target.innerText === '恢复')
+    {
+        const id = e.target.parentNode.dataset.id;
+        const thing = JSON.parse(localStorage.getItem(id));
+        localStorage.setItem(id,JSON.stringify({
+            id: id,
+            title: thing.title,
+            status: 'do'
+        }));
+        e.target.parentNode.outerHTML = '';
     }
 });
